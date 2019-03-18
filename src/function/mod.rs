61bfd3da9d9b13d724a0dd7fed4326bae3f7d3a4
uuhan <xuminhui189@gmail.com>
2019-03-19 02:25:22 +0800

    wip

--- src/function/mod.rs
+++ src/function/mod.rs
@@ -1,4 +1,18 @@
+#![allow(non_camel_case_types, unused)]
 pub use crate::v8::raw::Function;
 pub use crate::v8::raw::FunctionTemplate;
 pub use crate::v8::raw::FunctionCallback;
 pub use crate::v8::raw::FunctionCallbackInfo;
+
+// static kHolderIndex: i8 = 0;
+// static kIsolateIndex: i8 = 1;
+// static kReturnValueDefaultValueIndex: i8 = 2;
+// static kReturnValueIndex: i8 = 3;
+// static kDataIndex: i8 = 4;
+// static kNewTargetIndex: i8 = 5;
+
+impl FunctionCallbackInfo {
+    pub fn isolate(&self) {
+        unimplemented!()
+    }
+}
--- src/mac.rs
+++ src/mac.rs
@@ -1,33 +1,16 @@
 macro_rules! deref {
     ($s:ident) => {
         impl Deref for $s {
-            type Target = raw::$s;
+            type Target = crate::v8::raw::$s;
             fn deref(&self) -> &Self::Target {
                 unsafe { &*self.0 }
             }
         }
 
         impl DerefMut for $s {
-            fn deref_mut(&mut self) -> &mut raw::$s {
+            fn deref_mut(&mut self) -> &mut crate::v8::raw::$s {
                 unsafe {
                     &mut *self.0
-                }
-            }
-        }
-    };
-
-    ($s:ident -> $handle:ident) => {
-        impl Deref for $s {
-            type Target = raw::$s;
-            fn deref(&self) -> &Self::Target {
-                unsafe { &*self.$handle }
-            }
-        }
-
-        impl DerefMut for $s {
-            fn deref_mut(&mut self) -> &mut raw::$s {
-                unsafe {
-                    &mut *self.$handle
                 }
             }
         }
