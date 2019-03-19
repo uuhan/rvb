77d4ce3f39da3f13101e622ed6ce8bdd2d1add10
uuhan <xuminhui189@gmail.com>
2019-03-19 20:49:52 +0800

    impl Local<Object> & Local<ObjectTemplate>

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -6,9 +6,46 @@ pub use array::*;
 pub use map::*;
 pub use set::*;
 
-pub use crate::v8::raw::Object;
-pub use crate::v8::raw::ObjectTemplate;
+pub use crate::v8::{
+    Local,
+    Value,
+    FunctionTemplate,
+};
+
+pub use crate::v8::raw::{
+    self,
+    Object,
+    ObjectTemplate,
+};
 
 use crate::v8::Isolated;
 
-impl Isolated for ObjectTemplate {}
+impl Local<ObjectTemplate> {
+    pub fn New() -> Self {
+        let isolate = Self::GetIsolate();
+        unsafe {
+            ObjectTemplate::New(isolate.0, Local::<FunctionTemplate>::Empty())
+        }
+    }
+}
+
+impl Local<Object> {
+    pub fn New() -> Self {
+        let isolate = Self::GetIsolate();
+        unsafe {
+            Object::New(isolate.0)
+        }
+    }
+
+    pub fn set(&mut self, key: Local<Value>, value: Local<Value>) -> bool {
+        unsafe {
+            self.Set(key, value)
+        }
+    }
+
+    pub fn get(&mut self, key: Local<Value>) -> Local<Value> {
+        unsafe {
+            self.Get(key)
+        }
+    }
+}
