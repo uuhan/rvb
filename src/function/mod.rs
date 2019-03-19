efc1fcf2e291acc8672aae563c2a34ccc967dbc1
uuhan <xuminhui189@gmail.com>
2019-03-19 21:07:00 +0800

    impl Local<FunctionTemplate>

--- src/function/mod.rs
+++ src/function/mod.rs
@@ -1,12 +1,20 @@
 #![allow(non_camel_case_types, unused)]
+pub use crate::v8::raw::Signature;
 pub use crate::v8::raw::Function;
 pub use crate::v8::raw::FunctionTemplate;
 pub use crate::v8::raw::FunctionCallback;
 pub use crate::v8::raw::FunctionCallbackInfo;
 
+pub use crate::v8::raw::ConstructorBehavior_kThrow;
+pub use crate::v8::raw::ConstructorBehavior_kAllow;
+pub use crate::v8::raw::SideEffectType_kHasSideEffect;
+pub use crate::v8::raw::SideEffectType_kHasNoSideEffect;
+pub use crate::v8::raw::SideEffectType_kHasSideEffectToReceiver;
+
 use crate::v8::{
     Isolated,
     Local,
+    Value,
 };
 
 // static kHolderIndex: i8 = 0;
@@ -23,6 +31,23 @@ impl FunctionCallbackInfo {
 }
 
 impl Isolated for FunctionTemplate {}
+
+impl Local<FunctionTemplate> {
+    pub fn New() -> Self {
+        let isolate = Self::GetIsolate();
+        unsafe {
+            FunctionTemplate::New(
+                isolate.0,
+                None,
+                Local::<Value>::Empty(),
+                Local::<Signature>::Empty(),
+                0,
+                ConstructorBehavior_kAllow,
+                SideEffectType_kHasSideEffect,
+                )
+        }
+    }
+}
 
 impl Local<Function> {
     pub fn New() -> Self {
